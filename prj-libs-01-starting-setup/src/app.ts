5e72bfb188f4ab892b00da76f88120f087cffa8c
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2023-06-11 21:23:03 -0400

    170. Getting User Input

--- prj-libs-01-starting-setup/src/app.ts
+++ prj-libs-01-starting-setup/src/app.ts
@@ -1,1 +1,13 @@
 // Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // Send it to Google API
+  console.log(enteredAddress);
+}
+
+form.addEventListener("submit", searchAddressHandler);
